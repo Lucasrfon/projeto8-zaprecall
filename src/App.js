4f78878d7f74243f3c8c938a26218030df78f0c9
Lucas Fonseca <lucasrfon@gmail.com>
2022-05-12 01:14:22 -0300

    Tela inicial finalizada

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
+import React from 'react';
+import HomePage from "./HomePage";
+import MainPage from './MainPage';
+
 export default function App () {
+    const [screen, setScreen] = React.useState('HomePage')
+
     return (
+
         <>
+
+        {screen === 'HomePage' ? <HomePage setScreen={setScreen} /> : <MainPage />}
+
         </>
     )
 }
---
+++ src/HomePage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+export default function HomePage ({setScreen}) {
+    return (
+
+        <div>
+            <img src="./Imagens/logo.png" alt="Logo" />
+            <h1>ZapRecall</h1>
+            <button onClick={() => setScreen('MainPage')}>Iniciar Recall!</button>
+        </div>
+        
+    )
+}
