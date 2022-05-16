e2851eadbbcf2d9b3fc53f34290c55709de00d35
Lucas Fonseca <lucasrfon@gmail.com>
2022-05-16 17:45:42 -0300

    css colocado no src e fazendo ultimo bonus

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import "./css/reset.css";
+import "./css/style.css";
 import HomePage from "./HomePage";
 import MainPage from './MainPage';
 
--- src/HomePage.js
+++ src/HomePage.js
@@ -1,8 +1,8 @@
 import React from 'react'
 
-
 export default function HomePage ({setScreen, setMeta}) {
     const [botao, setBotao] = React.useState('off');
+    const decks = ["Deck1", "Deck2", "Deck3", "Deck4"]
 
     function definirMeta (event) {
         let aux = event.target.value;
@@ -16,6 +16,13 @@ export default function HomePage ({setScreen, setMeta}) {
         <div className="homeContainer">
             <img src="./Imagens/logo.png" alt="Logo" />
             <h1>ZapRecall</h1>
+            <input list="decks" placeholder="Escolha seu deck"/>
+                <datalist id="decks">
+                    <option value={decks[0]} />
+                    <option value={decks[1]} />
+                    <option value={decks[2]} />
+                    <option value={decks[3]} />
+                </datalist>
             {botao === 'off' 
             ? 
             <input type='number' min="1" max="8" step="1" placeholder='Digite sua meta de zaps...' onChange={definirMeta}></input> 
